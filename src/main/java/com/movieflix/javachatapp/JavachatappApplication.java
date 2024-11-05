e930c054f2a8da8d106f1ec5e8ec7781350982d1
hieunm2 <harrynguyen610@gmail.com>
2024-11-05 20:11:59 +0700

    init commit

---
+++ src/main/java/com/movieflix/javachatapp/JavachatappApplication.java
@@ -0,0 +1,13 @@
+package com.movieflix.javachatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavachatappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavachatappApplication.class, args);
+    }
+
+}
